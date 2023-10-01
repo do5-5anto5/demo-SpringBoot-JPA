0ab265b8b56beb6ce368b8bc82fd9596ba3845e7
do5-5anto5 <dos.santos.2k23@gmail.com>
2023-10-01 18:52:45 -0300

    Exceptin handling - delete

--- src/main/java/com/quinto/demo/services/UserService.java
+++ src/main/java/com/quinto/demo/services/UserService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import com.quinto.demo.entities.User;
 import com.quinto.demo.repositories.UserRepository;
 import com.quinto.demo.services.exceptions.ResourceNotFoundException;
+import com.quinto.demo.services.exceptions.DatabaseException;
 
 @Service
 public class UserService {
@@ -37,7 +40,13 @@ public class UserService {
 
 	@DeleteMapping(value = "/{id}")
 	public void delete (Long id) {
+		try {
 		repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	@PostMapping
